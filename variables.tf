a5a3f46a771de17ab1bc59e93c0f6abbd7d215bc
Emmylong1 <Emmanueliboke505@gmail.com>
2023-12-23 22:05:21 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+  default     = "modular-edge-183716"
+}
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "demo-cluster-1"
+}
+variable "env_name" {
+  description = "The environment for the GKE cluster"
+  default     = "prod"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-west1"
+}
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
+variable "project" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
